7b0d568fdfcc5e282adb168f0237c99409699266
Ragen Maharjan0 <ragenmah99@gmail.com>
2023-09-01 14:54:18 +0545

    github actions test 19

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
+
+          // scan qr code
+          // driver collects job
+          // auction is not showm ion dashboard
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
